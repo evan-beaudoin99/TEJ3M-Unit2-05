e25fd679e3cba2d09edfb2ad43f2396f47235a73
Evan Beaudoin <124371659+evan-beaudoin99@users.noreply.github.com>
2023-03-21 10:09:04 -0400

    Create servo.py

---
+++ servo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Created by: Evan Beaudoin.
+Created on: March 2023
+This program calculates the cost of a pizza.
+"""
+
+import time
+import board
+import pwmio
+from adafruit_motor import servo
+
+pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+
+my_servo = servo.Servo(pwm)
+
+def main() -> None: 
+    while True:
+        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+            my_servo.angle = angle
+            time.sleep(0.05)
+        for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+            my_servo.angle = angle
+            time.sleep(0.05)
+        
+if __name__ == "__main__":
+    main()
